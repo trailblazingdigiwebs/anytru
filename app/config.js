8f129fa5d98ee04d801eb7de8cc14d5d2796bfbb
trailblazingdigiwebs <trailblazingdigiwebs@gmail.com>
2024-10-10 13:49:58 +0530

    code submit

--- app/config.js
+++ app/config.js
@@ -1,13 +1,8 @@
 // config.js
 const config = {
-    apiBaseUrl: 'https://anytru-server.onrender.com',
-    domainUrl: 'https://anytru.ai',
+    apiBaseUrl: 'http://localhost:5000',
+    domainUrl: 'http://localhost:3000',
 
-    // apiBaseUrl: 'http://localhost:5000',
-    // domainUrl: 'http://localhost:3000',
-
-    // apiBaseUrl: 'http://10.5.49.96:5000',
-    // domainUrl: 'http://10.5.49.96:3000',
     
   };
   
